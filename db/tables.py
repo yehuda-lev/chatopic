f6a451bab1790d3e790f00ed3add5d193df384fd
Yehuda Lev <y124090@gmail.com>
2023-03-09 21:37:58 +0200

    [db.tables] add table Ids

--- db/tables.py
+++ db/tables.py
@@ -15,5 +15,14 @@ class Users(db.Entity):
     bio_user = Optional(str)
 
 
+class Ids(db.Entity):
+    tg_id = Required(str, unique=True)
+    topic_id = Required(int, unique=True)
+    user_msg_id = Required(int)
+    topic_msg_id = Required(int)
+
+
+
+
 db.bind(provider='sqlite', filename='chat_bot.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
