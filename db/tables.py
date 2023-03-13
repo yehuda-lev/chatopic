071d3088595094936f7de25c03c92ae231ec9dc0
Yehuda Lev <y124090@gmail.com>
2023-03-14 00:03:18 +0200

    [db.tables] add tg_id and topic_id in table Messages and i changed a little TgUser and add param messages in table TgTopic

--- db/tables.py
+++ db/tables.py
@@ -9,24 +9,46 @@ class TgGroup(db.Entity):
     topics = Set(lambda: TgTopic, reverse='group')
     admins = Set(lambda: TgUser, reverse='group')
 
+# class TgTopic(db.Entity):
+#     _table_ = 'tg_topic'
+#     id = PrimaryKey(int)
+#     group = Required(TgGroup, reverse='topics')
+#     name = Required(str)
+#     user = Optional(lambda: TgUser, reverse='topic')
+
 class TgTopic(db.Entity):
     _table_ = 'tg_topic'
     id = PrimaryKey(int)
     group = Required(TgGroup, reverse='topics')
     name = Required(str)
     user = Optional(lambda: TgUser, reverse='topic')
+    messages = Set(lambda: Message, reverse='topic_id')
+
 
 class TgUser(db.Entity):
     _table_ = 'tg_user'
     id = PrimaryKey(str)
     topic = Required(TgTopic, reverse='user')
     group = Optional(TgGroup, reverse='admins')
-    messages = Set(lambda: Message, reverse='sender')
+    messages = Set(lambda: Message, reverse='tg_id')
+
+# class TgUser(db.Entity):
+#     _table_ = 'tg_user'
+#     id = PrimaryKey(str)
+#     topic = Required(TgTopic, reverse='user')
+#     group = Optional(TgGroup, reverse='admins')
+#     messages = Set(lambda: Message, reverse='sender')
 
 
+# class Message(db.Entity):
+#     _table_ = 'tg_message'
+#     sender = Optional(TgUser)
+#     user_msg_id = Required(int)
+#     topic_msg_id = Required(int)
 class Message(db.Entity):
     _table_ = 'tg_message'
-    sender = Optional(TgUser)
+    tg_id = Optional(TgUser)
+    topic_id = Optional(TgTopic)
     user_msg_id = Required(int)
     topic_msg_id = Required(int)
 
