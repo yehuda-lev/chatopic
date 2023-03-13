0942c9413163d246a4fc74058e7c77cff270b52b
Yehuda Lev <y124090@gmail.com>
2023-03-14 00:04:08 +0200

    change table and filters

--- db/filters.py
+++ db/filters.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from pony.orm import db_session, select
-from db.tables import TgGroup, TgTopic, TgUser
+from db.tables import TgGroup, TgTopic, TgUser, Message
 
 
 # @db_session
@@ -10,51 +10,68 @@ from db.tables import TgGroup, TgTopic, TgUser
 
 
 @db_session
-def get_user_from_tg_id(tg_id: int):
-    return TgUser.get(tg_id=str(tg_id))
+def is_group_exists(group_id: int) -> bool:
+    return TgGroup.exists(id=str(group_id))
 
-
-@db_session
-def get_user_from_topic(topic_id: int):
-    return TgUser.get(topic_id=topic_id)
 
 @db_session
 def is_tg_id_exists(tg_id: int) -> bool:
-    return TgUser.exists(tg_id=str(tg_id))
+    return TgUser.exists(id=str(tg_id))
 
 @db_session
 def is_topic_id_exists(topic_id: int) -> bool:
-    return TgUser.exists(topic_id=topic_id)
-
-
-def get_topic_id(tg_id: int):
-    if get_user_from_tg_id(tg_id=tg_id) is None:
-        return None
-    return get_user_from_tg_id(tg_id=tg_id).topic_id
-
-
-def get_tg_id(topic_id: int):
-    if get_user_from_topic(topic_id=topic_id) is None:
-        return None
-    return get_user_from_topic(topic_id=topic_id).tg_id
-
+    return TgUser.exists(topic=topic_id)
 
 @db_session
 def create_user(tg_id: int, group_id: int, topic_id: int, name: Optional[str]) -> TgUser:
-    group = TgGroup(id=str(group_id))
-    topic = TgTopic(id=topic_id, group=group, name=name)
-    return TgUser(id=str(tg_id), topic=topic, group=group)
+    if not is_group_exists(group_id=group_id):
+        TgGroup(id=str(group_id))
+    topic = TgTopic(id=topic_id, group=str(group_id), name=name)
+    return TgUser(id=str(tg_id), topic=topic, group=str(group_id))
+
+
+@db_session
+def get_user_by_tg_id(tg_id: int):
+    return TgUser.get(id=str(tg_id))
+
+
+def get_group_by_tg_id(tg_id: int):
+    if get_user_by_tg_id(tg_id=tg_id) is None:
+        return None
+    return get_user_by_tg_id(tg_id=tg_id).group.id
+
+
+def get_topic_id_by_tg_id(tg_id: int):
+    if get_user_by_tg_id(tg_id=tg_id) is None:
+        return None
+    return get_user_by_tg_id(tg_id=tg_id).topic.id
+
+
+@db_session
+def get_user_by_topic(topic_id: int):
+    return TgUser.get(topic=topic_id)
+
+
+def get_tg_id_by_topic(topic_id: int):
+    if get_user_by_topic(topic_id=topic_id) is None:
+        return None
+    return get_user_by_topic(topic_id=topic_id).id
+
+
+@db_session
+def create_message(sender: int, user_msg_id: int, topic_msg_id: int):
+    user = get_user_by_tg_id(tg_id=sender)
+    return Message(sender=user, user_msg_id=user_msg_id, topic_msg_id=topic_msg_id)
+
+@db_session
+def get_user_from_user_msg_id(sender: int, msg_id: int):
+    return Message.get(sender=str(sender), user_msg_id=msg_id)
 
 @db_session
 def get_topic_msg_id_from_user_msg_id(tg_id: int, msg_id: int):
-    return Ids.get(tg_id=str(tg_id), user_msg_id=msg_id).topic_msg_id
+    return get_user_from_user_msg_id(sender=tg_id, msg_id=msg_id).topic_msg_id
 
 
 @db_session
-def get_tg_id_msg_id_from_topic_msg_id(topic_id: int, msg_topic_id: int):
-    return Ids.get(topic_id=topic_id, topic_msg_id=msg_topic_id).user_msg_is
-
-
-@db_session
-def create_ids(tg_id: int, topic_id: int, user_msg_id: int, topic_msg_id):
-    return Ids(tg_id=str(tg_id), topic_id=topic_id, user_msg_id=user_msg_id, topic_msg_id=topic_msg_id)
+def get_tg_id_msg_id_from_topic_msg_id(topic_id: int, msg_id: int):
+    return get_user_from_user_msg_id(sender=topic_id, msg_id=msg_id)
