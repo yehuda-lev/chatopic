3569a9618401368e72eb0f26de0e73a11e647069
Yehuda Lev <y124090@gmail.com>
2023-03-14 16:25:39 +0200

    [db.tables] delete # comments

--- db/tables.py
+++ db/tables.py
@@ -9,12 +9,6 @@ class TgGroup(db.Entity):
     topics = Set(lambda: TgTopic, reverse='group')
     admins = Set(lambda: TgUser, reverse='group')
 
-# class TgTopic(db.Entity):
-#     _table_ = 'tg_topic'
-#     id = PrimaryKey(int)
-#     group = Required(TgGroup, reverse='topics')
-#     name = Required(str)
-#     user = Optional(lambda: TgUser, reverse='topic')
 
 class TgTopic(db.Entity):
     _table_ = 'tg_topic'
@@ -32,19 +26,7 @@ class TgUser(db.Entity):
     group = Optional(TgGroup, reverse='admins')
     messages = Set(lambda: Message, reverse='tg_id')
 
-# class TgUser(db.Entity):
-#     _table_ = 'tg_user'
-#     id = PrimaryKey(str)
-#     topic = Required(TgTopic, reverse='user')
-#     group = Optional(TgGroup, reverse='admins')
-#     messages = Set(lambda: Message, reverse='sender')
 
-
-# class Message(db.Entity):
-#     _table_ = 'tg_message'
-#     sender = Optional(TgUser)
-#     user_msg_id = Required(int)
-#     topic_msg_id = Required(int)
 class Message(db.Entity):
     _table_ = 'tg_message'
     tg_id = Required(TgUser)
