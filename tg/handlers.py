de3855f63f56636f70b2cc865c3690a90be52818
Yehuda Lev <y124090@gmail.com>
2023-03-15 21:27:48 +0200

    [db.filters] change create message

--- tg/handlers.py
+++ tg/handlers.py
@@ -4,7 +4,8 @@ from pyrogram.raw.types import InputChannel
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 from db import filters
-from db.filters import is_tg_id_exists, get_group_by_tg_id, get_topic_id_by_tg_id
+from db.filters import is_tg_id_exists, get_group_by_tg_id, get_topic_id_by_tg_id, get_my_group, create_message, \
+    is_topic_id_exists, get_tg_id_by_topic
 
 
 async def create_topic(cli: Client, msg: Message):
@@ -36,3 +37,36 @@ async def is_user_exists(c: Client, msg: Message):
         topic_id = create.id
         print(f"create topic; group: {group_id}, chat_id: {msg.chat.id}, topic: {topic_id}, tg_id: {tg_id}")
         filters.create_user(tg_id=tg_id, group_id=group_id, topic_id=topic_id, name=name)
+
+
+async def forward_message(cli: Client, msg: Message):
+    print(get_my_group()[0])
+    print(msg.chat.id)
+    tg_id = msg.from_user.id
+    if msg.chat.id == tg_id:
+        print("user")
+        await is_user_exists(c=cli, msg=msg)
+        topic_id = get_topic_id_by_tg_id(tg_id=tg_id)
+        group = get_group_by_tg_id(tg_id=tg_id)
+        print("group_id:", group, "topic_id:", topic_id)
+        # try:
+        forward = await msg.copy(chat_id=int(group), reply_to_message_id=topic_id)
+        print(forward.id, "vs", msg.id)
+        create_message(tg_id_or_topic_id=tg_id, is_topic_id=False,
+                       user_msg_id=msg.id, topic_msg_id=forward.id)
+        # except Exception as e:
+        #     print(e)
+    elif msg.chat.id == int(get_my_group()[0]):
+        print("group")
+        topic_id = msg.reply_to_top_message_id if msg.reply_to_top_message_id else msg.reply_to_message_id
+        if not msg.reply_to_top_message_id or msg.reply_to_message_id:
+            return
+        if not is_topic_id_exists(topic_id=topic_id):
+            return
+        tg_id = get_tg_id_by_topic(topic_id=topic_id)
+        forward = await msg.copy(chat_id=tg_id)
+        print(forward.id, msg.id)
+        print(type(forward.id), type(msg.id))
+        create_message(tg_id_or_topic_id=topic_id, is_topic_id=True, user_msg_id=forward.id, topic_msg_id=msg.id)
+    else:
+        print("other")
