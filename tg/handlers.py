9ca34f04bc2f84101447b802ffd2aa38c281a8f0
Yehuda Lev <y124090@gmail.com>
2023-03-16 22:06:05 +0200

    [tg.handlers] deleted try except

--- tg/handlers.py
+++ tg/handlers.py
@@ -11,31 +11,27 @@ from db.filters import is_tg_id_exists, get_group_by_tg_id, get_topic_id_by_tg_i
 async def create_topic(cli: Client, msg: Message):
     name = msg.from_user.first_name + (" " + last if (last := msg.from_user.last_name) else "")
     username = username if (username:= msg.from_user.username) else "None"
-    try:
-        peer = await cli.resolve_peer(-1001558142106)
-        create = await cli.invoke(functions.channels.CreateForumTopic(
-            channel=InputChannel(channel_id=peer.channel_id, access_hash=peer.access_hash),
-            title=name,
-            random_id=1000000,
-            icon_color=None,
-            icon_emoji_id=5312016608254762256,
-            send_as=None
-            )
+    peer = await cli.resolve_peer(-1001558142106)
+    create = await cli.invoke(functions.channels.CreateForumTopic(
+        channel=InputChannel(channel_id=peer.channel_id, access_hash=peer.access_hash),
+        title=name,
+        random_id=1000000,
+        icon_color=None,
+        icon_emoji_id=5312016608254762256,
+        send_as=None
         )
-        await cli.send_message(chat_id=int("-100" + str(create.updates[1].message.peer_id.channel_id)),
-                               text=f"**Name:** [{name}](tg://user?id={msg.from_user.id})" \
-                                                        + f"\n**Username:** @{username}",
-                               reply_to_message_id=create.updates[1].message.id)
-        return create.updates[1].message
-    except Exception as e:
-        print("create_topic", e)
-        return
-
+    )
+    await cli.send_message(chat_id=int("-100" + str(create.updates[1].message.peer_id.channel_id)),
+                           text=f"**INFO THE USER**\n"
+                                f"**Name:** [{name}](tg://user?id={msg.from_user.id})" \
+                                f"\n**Username:** @{username}\n"
+                                f"**ID:** `{msg.from_user.id}`",
+                           reply_to_message_id=create.updates[1].message.id)
+    return create.updates[1].message
 
 async def is_user_exists(c: Client, msg: Message):
     name = msg.from_user.first_name + (" " + last if (last := msg.from_user.last_name) else "")
     tg_id = msg.from_user.id
-    print("tg_id exists:", filters.is_tg_id_exists(tg_id=tg_id))
     if filters.is_tg_id_exists(tg_id=tg_id):
         return
     else:
@@ -56,9 +52,6 @@ async def forward_message_from_user(cli: Client, msg: Message):
     except BadRequest as e:
         print("forward_message_from_user", e.value)
         return
-    except Exception as e:
-        print("forward_message_from_user:", e)
-        return
 
 
 async def forward_message_from_topic(cli: Client, msg: Message):
@@ -68,15 +61,21 @@ async def forward_message_from_topic(cli: Client, msg: Message):
     if not is_topic_id_exists(topic_id=topic_id):
         return
     tg_id = get_tg_id_by_topic(topic_id=topic_id)
-    try:
-        forward = await msg.copy(chat_id=tg_id)
-        print(forward.id, msg.id)
-        print(type(forward.id), type(msg.id))
-        create_message(tg_id_or_topic_id=topic_id, is_topic_id=True,
-                       user_msg_id=forward.id, topic_msg_id=msg.id)
-    except Exception as e:
-        print(e)
-        return
+    forward = await msg.copy(chat_id=tg_id)
+    print(forward.id, msg.id)
+    print(type(forward.id), type(msg.id))
+    create_message(tg_id_or_topic_id=topic_id, is_topic_id=True,
+                   user_msg_id=forward.id, topic_msg_id=msg.id)
+
+
+async def forward_message(cli: Client, msg: Message):
+    tg_id = msg.from_user.id
+    if msg.chat.id == tg_id:
+        await forward_message_from_user(cli=cli, msg=msg)
+    elif msg.chat.id == int(get_my_group()[0]):
+        await forward_message_from_topic(cli=cli, msg=msg)
+    else:
+        print("other")
 
 
 async def forward_message(cli: Client, msg: Message):
