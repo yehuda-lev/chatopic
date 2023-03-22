e28898f7feae474b482f6743a77b1ea445749318
Yehuda Lev <y124090@gmail.com>
2023-03-22 19:13:17 +0200

    [tg.filters] add filter: is_have_a_group

--- tg/filters.py
+++ tg/filters.py
@@ -1,6 +1,6 @@
 from pyrogram.types import Message
 
-from db.filters import is_admin_exists
+from db.filters import is_admin_exists, check_if_have_a_group
 
 
 def is_not_raw(_, __, msg: Message) -> bool:
@@ -15,3 +15,7 @@ def is_admin(_, __, msg: Message) -> bool:
     if is_admin_exists(tg_id=msg.from_user.id):
         return True
     return False
+
+
+def is_have_a_group(_, __, msg: Message):
+    return check_if_have_a_group()
