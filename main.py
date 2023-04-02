e96c5059bf18cc6b7a08ea05b3f5239cceeca237
Yehuda Lev <y124090@gmail.com>
2023-04-03 01:09:04 +0300

    [tg.main] add func main

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from pyrogram import Client
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from tg import handlers
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    app = Client("my_bot")
+    for handler in handlers.HANDLERS:
+        app.add_handler(handler)
+    app.run()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
