9924de2540c834bd4a003880b490cb1f30ca2b58
Yehuda Lev <y124090@gmail.com>
2023-04-02 20:39:41 +0300

    [db.filters] fix syntax

--- db/filters.py
+++ db/filters.py
@@ -14,29 +14,36 @@ def check_if_have_a_group() -> bool:
         return True
     return False
 
+
 @db_session
 def is_group_exists(group_id: int) -> bool:
     return TgGroup.exists(id=str(group_id))
+
 
 @db_session
 def is_admin_exists(tg_id: int) -> bool:
     return Admin.exists(id=str(tg_id))
 
+
 @db_session
 def is_tg_id_exists(tg_id: int) -> bool:
     return TgUser.exists(id=str(tg_id))
+
 
 @db_session
 def is_topic_id_exists(topic_id: int) -> bool:
     return TgTopic.exists(id=topic_id)
 
+
 @db_session
 def create_admin(tg_id: int):
     return Admin(id=str(tg_id))
 
+
 @db_session
 def create_group(group_id: int, name: str):
     return TgGroup(id=str(group_id), name=name)
+
 
 @db_session
 def create_user(tg_id: int, group_id: int, topic_id: int, name: Optional[str]):
@@ -50,12 +57,14 @@ def create_user(tg_id: int, group_id: int, topic_id: int, name: Optional[str]):
     topic = TgTopic(id=topic_id, group=str(group_id), name=name)
     return TgUser(id=str(tg_id), topic=topic, group=str(group_id))
 
+
 @db_session
 def get_my_group() -> TgGroup.id:
     try:
         return select(i.id for i in TgGroup)[:][0]
     except IndexError:
         return "not exists"
+
 
 @db_session
 def get_user_by_tg_id(tg_id: int):
@@ -75,21 +84,26 @@ def get_topic_id_by_tg_id(tg_id: int):
         return None
     return user.topic.id
 
+
 def get_is_protect(tg_id: int):
     return get_user_by_tg_id(tg_id=tg_id).protect
+
 
 @db_session
 def change_protect(tg_id: int, is_protect: bool):
     user = TgUser.get(id=str(tg_id))
     user.protect = is_protect
 
+
 def get_is_banned(tg_id: int):
     return get_user_by_tg_id(tg_id=tg_id).ban
+
 
 @db_session
 def change_banned(tg_id: int, is_banned: bool):
     user = TgUser.get(id=str(tg_id))
     user.ban = is_banned
+
 
 @db_session
 def get_user_by_topic(topic_id: int):
@@ -109,12 +123,13 @@ def create_message(tg_id_or_topic_id: int, is_topic_id: bool, user_msg_id: int, 
         topic_id, tg_id = tg_id_or_topic_id, get_tg_id_by_topic(topic_id=tg_id_or_topic_id)
     else:
         tg_id, topic_id = tg_id_or_topic_id, get_topic_id_by_tg_id(tg_id=tg_id_or_topic_id)
-    return Message(tg_id=str(tg_id) ,topic_id=topic_id, user_msg_id=user_msg_id, topic_msg_id=topic_msg_id)
+    return Message(tg_id=str(tg_id), topic_id=topic_id, user_msg_id=user_msg_id, topic_msg_id=topic_msg_id)
 
 
 @db_session
 def get_user_by_user_msg_id(tg_id: int, msg_id: int):
     return Message.get(tg_id=str(tg_id), user_msg_id=msg_id)
+
 
 @db_session
 def get_topic_msg_id_by_user_msg_id(tg_id: int, msg_id: int):
