6b33029391f11c5a85b7360aab5a5ea249b0c7ba
Yehuda Lev <y124090@gmail.com>
2023-04-03 02:21:59 +0300

    [tg.handlers] add param is_topic and is_banned

--- tg/handlers.py
+++ tg/handlers.py
@@ -8,20 +8,27 @@ from tg.forward import forward_message
 
 HANDLERS = [
     handlers.MessageHandler(get_info_command, pyrogram.filters.command("info")
-                            & pyrogram.filters.text & pyrogram.filters.group),
+                            & pyrogram.filters.text & pyrogram.filters.group
+                            & pyrogram.filters.create(tg_filters.is_topic)),
     handlers.MessageHandler(protect, pyrogram.filters.command(["protect", "unprotect"])
                             & pyrogram.filters.text & pyrogram.filters.group
+                            & pyrogram.filters.create(tg_filters.is_topic)
                             & pyrogram.filters.create(tg_filters.is_banned)),
     handlers.MessageHandler(ban_users, pyrogram.filters.command(["ban", "unban"])
-                            & pyrogram.filters.text & pyrogram.filters.group),
+                            & pyrogram.filters.text & pyrogram.filters.group
+                            & pyrogram.filters.create(tg_filters.is_topic)),
     handlers.MessageHandler(request_group, pyrogram.filters.command("add_group")
                             & pyrogram.filters.text & pyrogram.filters.create(tg_filters.is_admin)
-                            & ~ pyrogram.filters.create(tg_filters.is_have_a_group)),
+                            & pyrogram.filters.private & ~ pyrogram.filters.create(tg_filters.is_have_a_group)),
     handlers.MessageHandler(forward_message, ~ pyrogram.filters.command(
         ["start", "info", "protect", "unprotect", "ban", "unban", "add_group"])
                             & pyrogram.filters.create(tg_filters.is_not_raw)
                             & pyrogram.filters.create(tg_filters.is_have_a_group)
-                            & pyrogram.filters.create(tg_filters.is_user_exists)),
-    handlers.EditedMessageHandler(edited_message, pyrogram.filters.create(tg_filters.is_have_a_group)),
+                            & pyrogram.filters.create(tg_filters.is_topic)
+                            & pyrogram.filters.create(tg_filters.is_user_exists)
+                            & pyrogram.filters.create(tg_filters.is_banned)),
+    handlers.EditedMessageHandler(edited_message, pyrogram.filters.create(tg_filters.is_have_a_group)
+                                  & pyrogram.filters.create(tg_filters.is_topic)
+                                  & pyrogram.filters.create(tg_filters.is_banned)),
     handlers.RawUpdateHandler(create_group)
 ]
