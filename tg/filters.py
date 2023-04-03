1b6d4b0516decef793fb873bc21ea4ec31d88e55
Yehuda Lev <y124090@gmail.com>
2023-04-03 22:04:54 +0300

    [tg.filters] replace text in strings.py

--- tg/filters.py
+++ tg/filters.py
@@ -4,6 +4,7 @@ from pyrogram.raw import types as raw_types
 from pyrogram.types import Message
 
 from db import filters as db_filters
+from tg.strings import resolve_msg
 
 
 def is_banned(_, __, msg: Message):
@@ -12,8 +13,9 @@ def is_banned(_, __, msg: Message):
         if db_filters.get_is_banned(tg_id=tg_id):
             return False
     else:
+        # TODO get_is_banned_by_topic_id
         if db_filters.get_is_banned(tg_id=tg_id):
-            msg.reply("the user is ban\nYou can unban him by sending the /unban command")
+            msg.reply(resolve_msg(key='USER_IS_BANNED', msg_or_user=msg))
             return False
     return True
 
@@ -32,7 +34,7 @@ async def is_user_exists(_, c: Client, msg: Message):
 
 async def create_topic(cli: Client, msg: Message):
     name = msg.from_user.first_name + (" " + last if (last := msg.from_user.last_name) else "")
-    username = "@" + str(username) if (username := msg.from_user.username) else "אין"
+    username = "@" + str(username) if (username := msg.from_user.username) else "❌"
     peer = await cli.resolve_peer(int(db_filters.get_my_group()))
     create = await cli.invoke(functions.channels.CreateForumTopic(
         channel=raw_types.InputChannel(channel_id=peer.channel_id, access_hash=peer.access_hash),
@@ -43,11 +45,8 @@ async def create_topic(cli: Client, msg: Message):
         send_as=None
     )
     )
-    text = f"**פרטים על המשתמש**\n" \
-           f"**שם:** [{name}](tg://user?id={msg.from_user.id})" \
-           f"\n**שם משתמש:** {username}\n" \
-           f"‏**ID:** `{msg.from_user.id}`\n\n" \
-           f"לקבלת מידע על הפקודות הנתמכות בצאט אנא שלח את הפקודה /info"
+    text = resolve_msg(key='INFO_TOPIC', msg_or_user=msg).\
+        format(f"[{name}](tg://user?id={msg.from_user.id})", f"{username}", f"{msg.from_user.id}")
     photo = photo if (photo := msg.from_user.photo) else None
     chat_id = int("-100" + str(create.updates[1].message.peer_id.channel_id))
     if photo is None:
@@ -82,7 +81,7 @@ def is_not_raw(_, __, msg: Message) -> bool:
 
 def is_admin(_, __, msg: Message) -> bool:
     if not db_filters.is_admin_exists(tg_id=msg.from_user.id):
-        msg.reply("אינך מנהל")
+        msg.reply(resolve_msg(key='IS_ADMIN', msg_or_user=msg))
         return False
     return True
 
@@ -91,8 +90,8 @@ def is_have_a_group(_, __, msg: Message):
     if not db_filters.check_if_have_a_group():
         if db_filters.is_admin_exists(tg_id=msg.from_user.id):
             if not msg.command:
-                msg.reply("אין עדיין קבוצה, אני שלח את הפקודה /add_group")
+                msg.reply(resolve_msg(key='GROUP_NOT_EXISTS', msg_or_user=msg))
         else:
-            msg.reply("הבוט לא פועל עדיין אנא חזור שוב בהמשך")
+            msg.reply(resolve_msg(key='BOT_NOT_WORKING', msg_or_user=msg))
         return False
     return True
