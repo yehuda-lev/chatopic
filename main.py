593f971c111f32f0c0e6403a1d22df4853c2a4c9
Yehuda Lev <y124090@gmail.com>
2023-04-04 15:33:37 +0300

    [main] add os.environ.NAME

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import os
 from pyrogram import Client
-
+from dotenv import load_dotenv
+from db import filters as db_filters
 from tg import handlers
 
 
+load_dotenv()
+
+
 def main():
-    app = Client("my_bot")
+    app = Client(name=os.environ['PYROGRAM_NAME_SESSION'], api_id=os.environ['TELEGRAM_API_ID'],
+                 api_hash=os.environ['TELEGRAM_API_HASH'], bot_token=os.environ['TELEGRAM_BOT_TOKEN'])
+
+    for admin in os.environ['ADMINS'].split(','):
+        if not db_filters.is_admin_exists(tg_id=int(admin)):
+            db_filters.create_admin(tg_id=int(admin))
+
     for handler in handlers.HANDLERS:
         app.add_handler(handler)
+
     app.run()
 
 
