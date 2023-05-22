ccccecc4bd8080d681c82505853eaaf30240e9c2
Yehuda Lev <y124090@gmail.com>
2023-05-22 20:16:42 +0300

    [tg.forward] add documentation

--- tg/forward.py
+++ tg/forward.py
@@ -1,15 +1,43 @@
 import pyrogram
 from pyrogram import Client
-from pyrogram.errors import BadRequest, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
+from pyrogram.errors import (BadRequest, InputUserDeactivated, UserIsBlocked, PeerIdInvalid)
 from pyrogram.types import Message
 
 from db import filters as db_filters
 
 
-def get_reply_to_message_by_user(msg: Message):
+async def forward_message(cli: Client, msg: Message):
+    """
+    Forwarding a message sent from a user to a group
+    or from a group to a user
+    """
+
+    if msg.service or msg.game:  # you cant copy this message
+        return
+
+    tg_id = msg.from_user.id
+
+    if msg.chat.id == tg_id:  # the message sent by user
+        await forward_message_from_user(msg=msg)
+
+    elif db_filters.is_group_exists(group_id=msg.chat.id):  # the message sent by topic
+        await forward_message_from_topic(msg=msg)
+
+    else:
+        return
+
+
+def get_reply_to_message_by_user(msg: Message) -> int:
+    """
+    get reply to message by user,
+    if not reply: return topic id
+    """
+
     tg_id = msg.from_user.id
     if msg.reply_to_message:
-        is_reply = db_filters.get_topic_msg_id_by_user_msg_id(tg_id=tg_id, msg_id=msg.reply_to_message.id)
+        is_reply = db_filters.get_topic_msg_id_by_user_msg_id(
+            tg_id=tg_id, msg_id=msg.reply_to_message.id)
+
         if is_reply is not None:
             reply = is_reply
         else:
@@ -19,23 +47,15 @@ def get_reply_to_message_by_user(msg: Message):
     return reply
 
 
-def get_reply_to_message_by_topic(msg: Message):
-    topic_id = topic if (topic := msg.reply_to_top_message_id) else msg.reply_to_message_id
-    if msg.reply_to_message:
-        is_reply = db_filters.get_user_msg_id_by_topic_msg_id(topic_id=topic_id, msg_id=msg.reply_to_message.id)
-        if is_reply is not None:
-            reply = is_reply
-        else:
-            reply = None
-    else:
-        reply = None
-    return reply
+async def forward_message_from_user(msg: Message):
+    """
+    the message sent by user > forward message to topic
+    """
 
-
-async def forward_message_from_user(cli: Client, msg: Message):
     tg_id = msg.from_user.id
     group = db_filters.get_group_by_tg_id(tg_id=tg_id)
     reply = get_reply_to_message_by_user(msg=msg)
+
     try:
         forward = await msg.copy(chat_id=int(group), reply_to_message_id=reply)
         db_filters.create_message(tg_id_or_topic_id=tg_id, is_topic_id=False,
@@ -45,13 +65,38 @@ async def forward_message_from_user(cli: Client, msg: Message):
         return
 
 
-async def forward_message_from_topic(cli: Client, msg: Message):
+def get_reply_to_message_by_topic(msg: Message) -> int | None:
+    """
+    get reply to message by topic,
+    if not reply: return None
+    """
+
+    topic_id = topic if (topic := msg.reply_to_top_message_id) else msg.reply_to_message_id
+    if msg.reply_to_message:
+        is_reply = db_filters.get_user_msg_id_by_topic_msg_id(
+            topic_id=topic_id, msg_id=msg.reply_to_message.id)
+
+        if is_reply is not None:
+            reply = is_reply
+        else:
+            reply = None
+    else:
+        reply = None
+
+    return reply
+
+
+async def forward_message_from_topic(msg: Message):
+    """the message sent in topic > forward message to user"""
+
     topic_id = topic if (topic := msg.reply_to_top_message_id) else msg.reply_to_message_id
     tg_id = db_filters.get_tg_id_by_topic(topic_id=topic_id)
     is_protect = db_filters.get_is_protect_by_topic_id(topic_id=topic_id)
     reply = get_reply_to_message_by_topic(msg=msg)
+
     try:
-        forward = await msg.copy(chat_id=tg_id, reply_to_message_id=reply, protect_content=is_protect)
+        forward = await msg.copy(chat_id=tg_id, reply_to_message_id=reply,
+                                 protect_content=is_protect)
         db_filters.create_message(tg_id_or_topic_id=topic_id, is_topic_id=True,
                                   user_msg_id=forward.id, topic_msg_id=msg.id)
 
@@ -67,18 +112,3 @@ async def forward_message_from_topic(cli: Client, msg: Message):
     except BadRequest as e:
         db_filters.change_active(tg_id=tg_id, active=False)
         print(e)
-
-
-# @app.on_message(pyrogram.filters.create(is_not_raw) & pyrogram.filters.create(is_have_a_group))
-async def forward_message(cli: Client, msg: Message):
-    if msg.service or msg.game:
-        print("service")
-        return
-    tg_id = msg.from_user.id
-    if msg.chat.id == tg_id:
-        await forward_message_from_user(cli=cli, msg=msg)
-    elif db_filters.is_group_exists(group_id=msg.chat.id):
-        await forward_message_from_topic(cli=cli, msg=msg)
-    else:
-        print("other")
-        return
