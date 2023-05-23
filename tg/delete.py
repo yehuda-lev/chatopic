c5613961883cc6e7bf36c268ca67eff710d88236
Yehuda Lev <y124090@gmail.com>
2023-05-23 17:11:42 +0300

    [tg.delete] added except MessageDeleteForbidden

--- tg/delete.py
+++ tg/delete.py
@@ -1,4 +1,5 @@
 from pyrogram import Client
+from pyrogram.errors import MessageDeleteForbidden
 from pyrogram.types import Message
 
 from db import filters as db_filters
@@ -20,15 +21,18 @@ def get_reply_to_message_by_topic(msg: Message):
 def delete(c: Client, msg: Message):
     if db_filters.is_group_exists(group_id=msg.chat.id):
         reply = get_reply_to_message_by_topic(msg)
-        if reply:
-            topic_id = topic if (topic := msg.reply_to_top_message_id) else msg.reply_to_message_id
-            tg_id = db_filters.get_tg_id_by_topic(topic_id=topic_id)
-            c.delete_messages(chat_id=tg_id, message_ids=reply)
-            c.delete_messages(chat_id=msg.chat.id, message_ids=msg.reply_to_message.id)
-            c.delete_messages(chat_id=msg.chat.id, message_ids=msg.id)
-        else:
-            c.delete_messages(chat_id=msg.chat.id, message_ids=msg.reply_to_message.id)
-            c.delete_messages(chat_id=msg.chat.id, message_ids=msg.id)
+        try:
+            if reply:
+                topic_id = topic if (topic := msg.reply_to_top_message_id) else msg.reply_to_message_id
+                tg_id = db_filters.get_tg_id_by_topic(topic_id=topic_id)
+                c.delete_messages(chat_id=tg_id, message_ids=reply)
+                c.delete_messages(chat_id=msg.chat.id, message_ids=msg.reply_to_message.id)
+                c.delete_messages(chat_id=msg.chat.id, message_ids=msg.id)
+            else:
+                c.delete_messages(chat_id=msg.chat.id, message_ids=msg.reply_to_message.id)
+                c.delete_messages(chat_id=msg.chat.id, message_ids=msg.id)
+        except MessageDeleteForbidden as e:
+            print(e)
 
 
 
