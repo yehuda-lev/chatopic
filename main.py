b3e40e04724ff9decef43dd01f6df22982b7abf3
Yehuda Lev <y124090@gmail.com>
2023-05-30 02:42:21 +0300

    [main] added command for admin

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 from pyrogram import Client
 from dotenv import load_dotenv
+from pyrogram.errors import PeerIdInvalid
+from pyrogram.types import BotCommand, BotCommandScopeDefault, BotCommandScopeChat
+
 from db import filters as db_filters
 from tg import handlers
-
+from tg.strings import resolve_msg
 
 load_dotenv()
 
@@ -16,12 +19,31 @@ def main():
         if not db_filters.is_admin_exists(tg_id=int(admin)):
             db_filters.create_admin(tg_id=int(admin))
 
+        set_commands_for_admin(c=app, admin_id=int(admin))
+
     print(f"Bot {app.name} is up and running!")
 
     for handler in handlers.HANDLERS:
         app.add_handler(handler)
 
     app.run()
+
+
+def set_commands_for_admin(c: Client, admin_id: int):
+    c.start()
+    try:
+        c.set_bot_commands(
+            commands=[
+                BotCommand(command='start', description='start'),
+                BotCommand(command='add_group', description=resolve_msg('COMMAND_ADD_GROUP')),
+                BotCommand(command='delete_group', description=resolve_msg('COMMAND_DELETE_GROUP')),
+                BotCommand(command='send', description=resolve_msg('COMMAND_SEND'))
+            ],
+            scope=BotCommandScopeChat(chat_id=admin_id)
+        )
+    except PeerIdInvalid:
+        pass
+    c.stop()
 
 
 if __name__ == '__main__':
