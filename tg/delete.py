4a5f67344eda79c3029a2c621f6d6c211196fa05
Yehuda Lev <y124090@gmail.com>
2023-05-30 00:03:04 +0300

    [tg.delete] add func to delete message in group

--- tg/delete.py
+++ tg/delete.py
@@ -5,6 +5,30 @@ from pyrogram.types import Message
 from db import filters as db_filters
 
 
+def delete_message(c, msg):
+
+    if msg[0].chat is not None:  # check if msg delete in chat user or group
+        group = msg[0].chat.id
+
+        if db_filters.is_group_exists(group_id=group):
+
+            del_ids = [i.id for i in msg]  # list of id to msg delete
+            msg_ids = [db_filters.get_user_by_topic_msg_id2(i) for i in del_ids]  # list of Message (DB)
+
+            my_dict = {}
+            for msg in msg_ids:  # create dict{tg_id: [msg_id]}
+                if msg is not None:
+                    if my_dict.get(int(msg.tg_id.id)) is None:
+                        my_dict[int(msg.tg_id.id)] = [msg.user_msg_id]
+                    else:
+                        my_dict[int(msg.tg_id.id)].append(msg.user_msg_id)
+                else:
+                    continue
+
+            for user in my_dict.keys():
+                c.delete_messages(chat_id=user, message_ids=my_dict[user])
+
+
 def get_reply_to_message_by_topic(msg: Message):
     topic_id = topic if (topic := msg.reply_to_top_message_id) else msg.reply_to_message_id
     if msg.reply_to_message:
@@ -33,6 +57,5 @@ def delete(c: Client, msg: Message):
                 c.delete_messages(chat_id=msg.chat.id, message_ids=msg.id)
         except MessageDeleteForbidden as e:
             print(e)
-
 
 
