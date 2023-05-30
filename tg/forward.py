2d70c1555196e13f98b983432558d2d21aca0ed8
Yehuda Lev <y124090@gmail.com>
2023-05-30 03:55:58 +0300

    [tg.forward] I reduced the number of calls to the DB

--- tg/forward.py
+++ tg/forward.py
@@ -108,10 +108,10 @@ def get_reply_to_message_by_topic(msg: Message) -> int | None:
     if msg.reply_to_message:
         is_reply = db_filters.get_user_by_topic_msg_id(
             topic_id=topic_id,
-            msg_id=msg.reply_to_message.id).user_msg_id
+            msg_id=msg.reply_to_message.id)
 
         if is_reply is not None:
-            reply = is_reply
+            reply = is_reply.user_msg_id
         else:
             reply = None
     else:
